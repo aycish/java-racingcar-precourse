722789fef746b8b345108f12a55d66d9307f5869
aycish <aycish07@gmail.com>
2021-10-11 16:19:41 +0900

    refactor : ValidationUtils 테스트 코드 위치 변경

--- src/test/java/racinggame/domain/ValidationUtilsTest.java
+++ src/test/java/racinggame/domain/ValidationUtilsTest.java
@@ -1,4 +1,4 @@
-package racinggame;
+package racinggame.domain;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import racinggame.domain.ValidationUtils;
 
 public class ValidationUtilsTest {
 
