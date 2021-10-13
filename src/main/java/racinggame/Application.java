3b6b2df57854d78f61399cd2571da37b4bc52260
aycish <aycish07@gmail.com>
2021-10-13 13:44:40 +0900

    style: 주석 제거

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -4,7 +4,6 @@ import racinggame.controller.GameController;
 
 public class Application {
 	public static void main(String[] args) {
-		// TODO 자동차 경주 게임 구현
 		GameController gameController = new GameController();
 		gameController.startGame();
 	}
